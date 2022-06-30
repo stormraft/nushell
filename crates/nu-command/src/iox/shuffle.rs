f46e5a08aaa3236c8ce2cc5c823fead24cdff757
Michael Angerman <1809991+stormasm@users.noreply.github.com>
2022-06-29 20:46:24 -0700

    got tokio into Cargo.toml and it appears to be working

--- crates/nu-command/src/iox/shuffle.rs
+++ crates/nu-command/src/iox/shuffle.rs
@@ -6,6 +6,8 @@ use nu_protocol::{
 use rand::prelude::SliceRandom;
 use rand::thread_rng;
 
+use tokio::runtime::Runtime;
+
 #[derive(Clone)]
 pub struct Ioxshuffle;
 
@@ -44,5 +46,43 @@ impl Command for Ioxshuffle {
             example: r#"echo [[version patch]; [1.0.0 false] [3.0.1 true] [2.0.0 false]] | shuffle"#,
             result: None,
         }]
+    }
+}
+
+/// Creates the tokio runtime for executing IOx
+///
+/// if nthreads is none, uses the default scheduler
+/// otherwise, creates a scheduler with the number of threads
+fn get_runtime(num_threads: Option<usize>) -> Result<Runtime, std::io::Error> {
+    // NOTE: no log macros will work here!
+    //
+    // That means use eprintln!() instead of error!() and so on. The log emitter
+    // requires a running tokio runtime and is initialised after this function.
+
+    use tokio::runtime::Builder;
+    let kind = std::io::ErrorKind::Other;
+    match num_threads {
+        None => Runtime::new(),
+        Some(num_threads) => {
+            println!(
+                "Setting number of threads to '{}' per command line request",
+                num_threads
+            );
+
+            match num_threads {
+                0 => {
+                    let msg = format!(
+                        "Invalid num-threads: '{}' must be greater than zero",
+                        num_threads
+                    );
+                    Err(std::io::Error::new(kind, msg))
+                }
+                1 => Builder::new_current_thread().enable_all().build(),
+                _ => Builder::new_multi_thread()
+                    .enable_all()
+                    .worker_threads(num_threads)
+                    .build(),
+            }
+        }
     }
 }
