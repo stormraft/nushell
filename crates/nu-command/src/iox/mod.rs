11cb8a34a602122243c779a3a7a0565ce366f81b
Michael Angerman <1809991+stormasm@users.noreply.github.com>
2022-07-02 21:17:11 -0700

    commit delimited.rs and mod.rs

--- crates/nu-command/src/iox/mod.rs
+++ crates/nu-command/src/iox/mod.rs
@@ -1,9 +1,11 @@
+mod delimited;
 mod length;
 mod reverse;
 mod shuffle;
 mod sql;
 mod util;
 
+pub use delimited::*;
 pub use length::Ioxlength;
 pub use reverse::Ioxreverse;
 pub use shuffle::Ioxshuffle;
