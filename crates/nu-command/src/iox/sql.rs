93e9f4099023c5d387b53ba57479bfedd14570bf
Michael Angerman <1809991+stormasm@users.noreply.github.com>
2022-07-06 12:48:58 -0700

    cc remove all cruft, ioxsql is up and running

--- crates/nu-command/src/iox/sql.rs
+++ crates/nu-command/src/iox/sql.rs
@@ -1,14 +1,11 @@
+use super::delimited::from_delimited_data;
 use super::util::get_runtime;
 use nu_engine::CallExt;
 use nu_protocol::ast::Call;
 use nu_protocol::engine::{Command, EngineState, Stack};
-//use nu_protocol::IntoInterruptiblePipelineData;
 use nu_protocol::{
     Category, Example, PipelineData, ShellError, Signature, Span, Spanned, SyntaxShape, Value,
 };
-//use rand::prelude::SliceRandom;
-//use rand::thread_rng;
-use super::delimited::from_delimited_data;
 
 use csv::Trim;
 
@@ -50,48 +47,18 @@ impl Command for Ioxsql {
         let sql: Spanned<String> = call.req(engine_state, stack, 0)?;
         let db: Option<String> = call.get_flag(engine_state, stack, "dbname")?;
 
-        println!("{:?}", db);
-
         let dbname = if let Some(name) = db {
             name
         } else {
             std::env::var("IOX_DBNAME").unwrap()
         };
 
-        /*
-                let dbname = if let Some(name) = db {
-                    name
-                } else {
-                    "pears".to_string()
-                };
-
-
-        let db: Option<String> = call.opt(engine_state, stack, 0).unwrap_or(None);
-
-        let dbname = if let Some(name) = db {
-            name
-        } else {
-            std::env::var("INFLUXDB_IOX_CATALOG_DSN").unwrap()
-        };
-        */
-
-        //println!("dbname = {:?}", dbname);
-
-        // let dbname = std::env::var("INFLUXDB_IOX_CATALOG_DSN").unwrap();
-        // let dbname = "pears".to_string();
-
         let sql_result = tokio_block_sql(&dbname, &sql);
-
-        // println!("{:?}", sql_result);
 
         let no_infer = false;
         let noheaders = false;
-
         let separator: char = ',';
-
         let trim = Trim::None;
-
-        // let input = Value(String { val: "a,b,c\n1,2,10\n3,4,20\n", span: Span { start: 28605, end: 28611 } }, None);
 
         let input = PipelineData::Value(
             Value::String {
@@ -101,16 +68,6 @@ impl Command for Ioxsql {
             None,
         );
 
-        /* This works !!
-        let input = PipelineData::Value(
-            Value::String {
-                val: "a,b,c\n1,2,10\n3,4,20\n".to_string(),
-                span: call.head,
-            },
-            None,
-        );
-        */
-
         // This compiles and returns nothing
         // let input = PipelineData::Value(Value::Nothing { span: call.head }, None);
 
@@ -118,12 +75,6 @@ impl Command for Ioxsql {
         let config = engine_state.get_config();
 
         from_delimited_data(noheaders, no_infer, separator, trim, input, name, config)
-        /*
-        Ok(PipelineData::Value(
-            Value::Nothing { span: call.head },
-            None,
-        ))
-        */
     }
 
     fn examples(&self) -> Vec<Example> {
@@ -147,20 +98,11 @@ pub fn tokio_block_sql(dbname: &String, sql: &Spanned<String>) -> Result<String,
             .expect("client should be valid");
 
         let mut repl = Repl::new(connection);
-
         repl.use_database(dbname.to_string());
-        // repl.use_database("postgresql:///iox_shared".to_string());
-
         let _output_format = repl.set_output_format("csv");
 
-        let x = repl
-            //.run_sql("select * from h2o_temperature".to_string())
-            .run_sql(sql.item.to_string())
-            .await
-            .expect("run_sql");
-
-        // println!("{:?}", x);
-        x
+        let rsql = repl.run_sql(sql.item.to_string()).await.expect("run_sql");
+        rsql
     });
 
     Ok(sql_result)
