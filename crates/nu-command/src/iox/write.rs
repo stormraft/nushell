c5a784b724c0e4a3de9f11497b094b2737eed647
Michael Angerman <1809991+stormasm@users.noreply.github.com>
2022-07-06 19:59:06 -0700

    cc

--- crates/nu-command/src/iox/write.rs
+++ crates/nu-command/src/iox/write.rs
@@ -2,17 +2,10 @@ use super::util::get_runtime;
 use nu_engine::CallExt;
 use nu_protocol::ast::Call;
 use nu_protocol::engine::{Command, EngineState, Stack};
-//use nu_protocol::IntoInterruptiblePipelineData;
 
 use nu_protocol::{
     Category, Example, PipelineData, ShellError, Signature, Spanned, SyntaxShape, Value,
 };
-
-/*
-use nu_protocol::{
-    Category, Example, PipelineData, ShellError, Signature, Span, Spanned, SyntaxShape, Value,
-};
-*/
 
 #[derive(Clone)]
 pub struct Ioxwrite;
@@ -32,14 +25,14 @@ impl Command for Ioxwrite {
             .named(
                 "dbname",
                 SyntaxShape::String,
-                "name of the database to search over",
+                "name of the database to write to",
                 Some('d'),
             )
             .category(Category::Filters)
     }
 
     fn usage(&self) -> &str {
-        "Sql query against the Iox Database."
+        "Write data to the Iox Database."
     }
 
     fn run(
@@ -62,45 +55,6 @@ impl Command for Ioxwrite {
 
         println!("{:?}", nol_result);
 
-        /*
-        let no_infer = false;
-        let noheaders = false;
-
-        let separator: char = ',';
-
-        let trim = Trim::None;
-        */
-
-        // let input = Value(String { val: "a,b,c\n1,2,10\n3,4,20\n", span: Span { start: 28605, end: 28611 } }, None);
-
-        /*
-        let input = PipelineData::Value(
-            Value::String {
-                val: nol_result.unwrap(),
-                span: call.head,
-            },
-            None,
-        );
-        */
-
-        /* This works !!
-        let input = PipelineData::Value(
-            Value::String {
-                val: "a,b,c\n1,2,10\n3,4,20\n".to_string(),
-                span: call.head,
-            },
-            None,
-        );
-        */
-
-        // This compiles and returns nothing
-        // let input = PipelineData::Value(Value::Nothing { span: call.head }, None);
-
-        // let name = Span::new(0, 0);
-        // let config = engine_state.get_config();
-
-        // from_delimited_data(noheaders, no_infer, separator, trim, input, name, config)
-
         Ok(PipelineData::Value(
             Value::Nothing { span: call.head },
             None,
@@ -120,8 +74,6 @@ pub fn tokio_block_write(
     dbname: &String,
     lp_data: &Spanned<String>,
 ) -> Result<usize, std::io::Error> {
-    //use influxdb_iox_client::{connection::Builder, repl::Repl};
-
     use influxdb_iox_client::{connection::Builder, write::Client};
 
     let num_threads: Option<usize> = None;
@@ -135,36 +87,11 @@ pub fn tokio_block_write(
 
         let mut client = Client::new(connection);
 
-        // let _dbname_from_env = std::env::var("INFLUXDB_IOX_CATALOG_DSN").unwrap();
-
         let nol = client
             .write_lp(dbname.to_string(), lp_data.item.to_string(), 0)
             .await
             .expect("failed to write to IOx");
 
-        // write a line of line protocol data
-        /*
-        let nol = client
-            .write_lp("pears", "cpu,region=north user=50.32 20000000", 0)
-            .await
-            .expect("failed to write to IOx");
-            */
-
-        // let mut repl = Repl::new(connection);
-        // repl.use_database(dbname);
-        // repl.use_database("postgresql:///iox_shared".to_string());
-
-        //let _output_format = repl.set_output_format("csv");
-
-        /*
-        let x = repl
-            //.run_sql("select * from h2o_temperature".to_string())
-            .run_sql(sql.item.to_string())
-            .await
-            .expect("run_sql");
-            */
-        // println!("{:?}", x);
-        // x
         nol
     });
 
