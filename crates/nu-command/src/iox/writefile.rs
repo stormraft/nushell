8cbcbfa1a1939b2ba4f4e4bac846f74208a008ee
Michael Angerman <1809991+stormasm@users.noreply.github.com>
2022-07-07 18:41:14 -0700

    writefile is in there but not working (yet)

--- crates/nu-command/src/iox/writefile.rs
+++ crates/nu-command/src/iox/writefile.rs
@@ -1,11 +1,11 @@
-use super::util::tokio_block_write;
+use super::util::get_runtime;
 use nu_engine::CallExt;
 use nu_protocol::ast::Call;
 use nu_protocol::engine::{Command, EngineState, Stack};
+use std::fs::File;
+use std::io::Read;
 
-use nu_protocol::{
-    Category, Example, PipelineData, ShellError, Signature, Spanned, SyntaxShape, Value,
-};
+use nu_protocol::{Category, Example, PipelineData, ShellError, Signature, SyntaxShape, Value};
 
 #[derive(Clone)]
 pub struct Ioxwritefile;
@@ -18,9 +18,9 @@ impl Command for Ioxwritefile {
     fn signature(&self) -> nu_protocol::Signature {
         Signature::build("ioxwritefile")
             .required(
-                "data",
+                "filename",
                 SyntaxShape::String,
-                "Line protocol string to write to Iox",
+                "File name that contains influxdb line protocol data",
             )
             .named(
                 "dbname",
@@ -42,7 +42,7 @@ impl Command for Ioxwritefile {
         call: &Call,
         _input: PipelineData,
     ) -> Result<PipelineData, ShellError> {
-        let lp_data: Spanned<String> = call.req(engine_state, stack, 0)?;
+        let filename: String = call.req(engine_state, stack, 0)?;
         let db: Option<String> = call.get_flag(engine_state, stack, "dbname")?;
 
         let dbname = if let Some(name) = db {
@@ -53,7 +53,11 @@ impl Command for Ioxwritefile {
 
         println!("dbname = {:?}", dbname);
 
-        let nol_result = tokio_block_write(&dbname, &lp_data);
+        let mut file = File::open(filename).unwrap();
+        let mut lp_data = String::new();
+        let _ = file.read_to_string(&mut lp_data);
+
+        let nol_result = tokio_block_writefile(&dbname, &lp_data);
 
         println!("{:?}", nol_result);
 
@@ -77,4 +81,29 @@ impl Command for Ioxwritefile {
             },
         ]
     }
+}
+
+pub fn tokio_block_writefile(dbname: &String, lp_data: &String) -> Result<usize, std::io::Error> {
+    use influxdb_iox_client::{connection::Builder, write::Client};
+
+    let num_threads: Option<usize> = None;
+    let tokio_runtime = get_runtime(num_threads)?;
+
+    let nol_result = tokio_runtime.block_on(async move {
+        let connection = Builder::default()
+            .build("http://127.0.0.1:8081")
+            .await
+            .expect("client should be valid");
+
+        let mut client = Client::new(connection);
+
+        let nol = client
+            .write_lp(dbname.to_string(), lp_data.to_string(), 0)
+            .await
+            .expect("failed to write to IOx");
+
+        nol
+    });
+
+    Ok(nol_result)
 }
